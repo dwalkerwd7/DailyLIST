80609fdb3ca597bc0914d31d371d3cf1ead3480a
dewa9753 <dewa9753@colorado.edu>
2026-03-24 21:41:56 -0600

    made vite build to the correct outDIr instead of manually copying it over

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  const appBasePath = process.env.VITE_APP_BASE_PATH || '/'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const appBasePath = env.VITE_APP_BASE_PATH || '/'
   return {
     base: appBasePath,
+    build: {
+      outDir: `../server/public${appBasePath}`,
+    },
     plugins: [react(), tailwindcss()]
   }
 })
