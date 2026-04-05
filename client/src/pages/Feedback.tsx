eb5d69b6e03cf6243989c67e8a5f243fc2d8a9ac
Derek Walker <d.walkerwd7@gmail.com>
2026-04-05 11:45:04 -0600

    moved consts out of component

--- client/src/pages/Feedback.tsx
+++ client/src/pages/Feedback.tsx
@@ -3,149 +3,149 @@ import PageAlert, { type PageAlertProps } from '../components/utils/PageAlert';
 import { APIPaths } from '../app-constants';
 
 const successSubmitAlert: PageAlertProps = {
-  title: 'Feedback Submitted!',
-  msg: 'Thank you for your feedback!',
-  type: 'success'
+    title: 'Feedback Submitted!',
+    msg: 'Thank you for your feedback!',
+    type: 'success'
 };
 
 const errorSubmitAlert: PageAlertProps = {
-  title: 'Submission Failed',
-  msg: 'There was an issue submitting your feedback. Please try again later.',
-  type: 'error'
+    title: 'Submission Failed',
+    msg: 'There was an issue submitting your feedback. Please try again later.',
+    type: 'error'
 };
 
 const alreadySubmittedAlert: PageAlertProps = {
-  title: 'Feedback Submitted',
-  msg: 'Thank you for your feedback! We appreciate your input.',
-  type: 'info',
-  closable: false
+    title: 'Feedback Submitted',
+    msg: 'Thank you for your feedback! We appreciate your input.',
+    type: 'info',
+    closable: false
 };
 
 export default function Feedback() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
 
-  const [alertMessage, setAlertMessage] = useState<PageAlertProps>({ title: '', msg: '', type: 'success' });
-  const [hasLoaded, setHasLoaded] = useState(false);
-  const [hasSubmittedFeedback, setHasSubmittedFeedback] = useState(false);
+    const [alertMessage, setAlertMessage] = useState<PageAlertProps>({ title: '', msg: '', type: 'success' });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const [hasSubmittedFeedback, setHasSubmittedFeedback] = useState(false);
 
-  const submitFeedback = async () => {
-    try {
-      const response = await fetch(APIPaths.feedback, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
+    const submitFeedback = async () => {
+        try {
+            const response = await fetch(APIPaths.feedback, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
 
-      if(response.ok) {
-        setHasSubmittedFeedback(true);
-        setAlertMessage(successSubmitAlert);
-      } else {
-        setHasSubmittedFeedback(false);
-        setAlertMessage(errorSubmitAlert);
-      }
+            if (response.ok) {
+                setHasSubmittedFeedback(true);
+                setAlertMessage(successSubmitAlert);
+            } else {
+                setHasSubmittedFeedback(false);
+                setAlertMessage(errorSubmitAlert);
+            }
 
-    } catch(error) {
-      console.error('Error submitting feedback:', error);
-      setAlertMessage(errorSubmitAlert);
-    }
-  };
-
-  useEffect(() => {
-    const checkSubmissionStatus = async () => {
-      try {
-        const response = await fetch(APIPaths.feedback, {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' }
-        });
-        const data = await response.json();
-        setHasSubmittedFeedback(data.hasSubmitted);
-        if (data.hasSubmitted) {
-          setAlertMessage(alreadySubmittedAlert);
+        } catch (error) {
+            console.error('Error submitting feedback:', error);
+            setAlertMessage(errorSubmitAlert);
         }
-        setHasLoaded(true);
-      } catch (error) {
-        console.error('Error checking feedback submission status:', error);
-      }
     };
-    void checkSubmissionStatus();
-  }, []);
 
-  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    submitFeedback();
-  };
+    useEffect(() => {
+        const checkSubmissionStatus = async () => {
+            try {
+                const response = await fetch(APIPaths.feedback, {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json' }
+                });
+                const data = await response.json();
+                setHasSubmittedFeedback(data.hasSubmitted);
+                if (data.hasSubmitted) {
+                    setAlertMessage(alreadySubmittedAlert);
+                }
+                setHasLoaded(true);
+            } catch (error) {
+                console.error('Error checking feedback submission status:', error);
+            }
+        };
+        void checkSubmissionStatus();
+    }, []);
 
-  return (
-    <main className="mx-auto max-w-4xl flex-1 px-4 py-8 sm:px-6 lg:px-8 text-center">
-      <h1 className="text-4xl font-bold mb-4 text-primary-text">We'd love to hear from you!</h1>
-      <p className="text-lg text-primary-text mb-8">Your feedback helps us improve DailyLIST. Please share your thoughts, suggestions, or report any issues you encounter.</p>
-      <PageAlert
-        {...alertMessage}
-      />
-      { !hasLoaded && (
-        <p className="text-primary-text">Loading...</p>
-      )}
-      { !hasSubmittedFeedback && hasLoaded && (
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 justify-center">
-        <div className="flex flex-col">
-          <label htmlFor="name" className="block text-sm font-medium text-primary-text mb-1">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="bg-secondary-bg border border-form-input rounded-md py-2 px-4 text-primary-text focus:outline-none focus:ring-2 focus:ring-form-input"
-            placeholder="Your name"
-            required
-            maxLength={50}
-            value={formData.name}
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="email" className="block text-sm font-medium text-primary-text mb-1">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="bg-secondary-bg border border-form-input rounded-md py-2 px-4 text-primary-text focus:outline-none focus:ring-2 focus:ring-form-input"
-            placeholder="Your email"
-            required
-            maxLength={100}
-            value={formData.email}
-            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="message" className="block text-sm font-medium text-primary-text mb-1">
-            Message
-          </label>
-          <textarea
-            id="message"
-            rows={4}
-            className="bg-secondary-bg border border-form-input rounded-md py-2 px-4 text-primary-text focus:outline-none focus:ring-2 focus:ring-form-input"
-            placeholder="Your message"
-            required
-            maxLength={1000}
-            value={formData.message}
-            onChange={(e) => setFormData({ ...formData, message: e.target.value })}
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-gradient-to-r from-accent-from to-accent-to hover:from-accent-from-hover hover:to-accent-to-hover text-white font-bold py-2 px-4 rounded-md"
-        >
-          Submit Feedback
-        </button>
-      </form>
-      )}
-    </main>
-  );
+    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        submitFeedback();
+    };
+
+    return (
+        <main className="mx-auto max-w-4xl flex-1 px-4 py-8 sm:px-6 lg:px-8 text-center">
+            <h1 className="text-4xl font-bold mb-4 text-primary-text">We'd love to hear from you!</h1>
+            <p className="text-lg text-primary-text mb-8">Your feedback helps us improve DailyLIST. Please share your thoughts, suggestions, or report any issues you encounter.</p>
+            <PageAlert
+                {...alertMessage}
+            />
+            {!hasLoaded && (
+                <p className="text-primary-text">Loading...</p>
+            )}
+            {!hasSubmittedFeedback && hasLoaded && (
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4 justify-center">
+                    <div className="flex flex-col">
+                        <label htmlFor="name" className="block text-sm font-medium text-primary-text mb-1">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            className="bg-secondary-bg border border-form-input rounded-md py-2 px-4 text-primary-text focus:outline-none focus:ring-2 focus:ring-form-input"
+                            placeholder="Your name"
+                            required
+                            maxLength={50}
+                            value={formData.name}
+                            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="email" className="block text-sm font-medium text-primary-text mb-1">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="bg-secondary-bg border border-form-input rounded-md py-2 px-4 text-primary-text focus:outline-none focus:ring-2 focus:ring-form-input"
+                            placeholder="Your email"
+                            required
+                            maxLength={100}
+                            value={formData.email}
+                            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="message" className="block text-sm font-medium text-primary-text mb-1">
+                            Message
+                        </label>
+                        <textarea
+                            id="message"
+                            rows={4}
+                            className="bg-secondary-bg border border-form-input rounded-md py-2 px-4 text-primary-text focus:outline-none focus:ring-2 focus:ring-form-input"
+                            placeholder="Your message"
+                            required
+                            maxLength={1000}
+                            value={formData.message}
+                            onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="bg-linear-to-r from-accent-from to-accent-to hover:from-accent-from-hover hover:to-accent-to-hover text-white font-bold py-2 px-4 rounded-md"
+                    >
+                        Submit Feedback
+                    </button>
+                </form>
+            )}
+        </main>
+    );
 }
