419168676123275f47cb396a6b4631dff53b5c3d
Derek Walker <d.walkerwd7@gmail.com>
2026-04-06 14:50:24 -0600

    fixed widths of the main container element

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,16 @@
 import TodoApp from '../components/TodoApp';
 
 export default function Home() {
-  return (
-    <main className="mx-auto max-w-3xl sm:min-w-sm md:min-w-md flex-1 pt-8">
-      <div className="space-y-6">
-        <h1 className="text-4xl font-bold text-primary-text text-center">
-          Daily Todo List
-        </h1>
+    return (
+        <main className="w-full sm:max-w-xl md:max-w-3xl lg:max-w-4xl flex-1 self-center py-8">
+            <div className="space-y-6">
+                <h1 className="text-4xl font-bold text-primary-text text-center">
+                    Daily Todo List
+                </h1>
 
-        <TodoApp />
+                <TodoApp />
 
-      </div>
-    </main>
-  );
+            </div>
+        </main>
+    );
 }
