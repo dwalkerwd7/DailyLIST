80d4d4da47c52d4d75ff1164d2c25d7e588e81b3
Derek Walker <d.walkerwd7@gmail.com>
2026-04-06 14:32:04 -0600

    initial trial of making the todos wider on larger screens

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import TodoApp from '../components/TodoApp';
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-3xl flex-1 pt-8">
+    <main className="mx-auto max-w-3xl sm:min-w-sm md:min-w-md flex-1 pt-8">
       <div className="space-y-6">
         <h1 className="text-4xl font-bold text-primary-text text-center">
           Daily Todo List
