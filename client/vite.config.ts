0f191855023daa10d03861b2fc488fce17e8e397
Derek Walker <d.walkerwd7@gmail.com>
2026-04-07 16:29:45 -0600

    BASE_PATH now depends on dev or production mode, making the server code cleaner

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+    // if in production mode, prefix BASE_PATH if you're using it
+    const BASE_PATH = mode === 'production' ? (loadEnv(mode, process.cwd(), '').BASE_PATH ?? '/') : '/';
     return {
-        base: '/dailylist', // put whatever relative path that dailylist will be served on
+        base: BASE_PATH,
         build: {
             outDir: '../server/public',
             emptyOutDir: true,
