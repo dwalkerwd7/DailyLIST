9688ddb944efd24fb6fab131f79d701a5b4c730c
Derek Walker <d.walkerwd7@gmail.com>
2026-04-07 15:20:05 -0600

    trying this out

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(() => {
     return {
-        base: env.VITE_BASE_PATH,
+        base: '/',
+        build: {
+            outDir: '../server/public',
+            emptyOutDir: true,
+        },
         plugins: [react(), tailwindcss()]
     }
 })
