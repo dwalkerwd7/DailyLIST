208b491e2d0159c761f6e10154bc275f2cd58edc
Derek Walker <d.walkerwd7@gmail.com>
2026-04-07 15:11:42 -0600

    turns out, still need the base path of where this project is installed if not running it standalone

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,57 +7,57 @@ import About from './pages/About';
 import Feedback from './pages/Feedback';
 
 function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex min-h-screen flex-col bg-primary-bg">
-      <Header />
-        {children}
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="flex min-h-screen flex-col bg-primary-bg">
+            <Header />
+            {children}
+            <Footer />
+        </div>
+    );
 }
 
 function App() {
 
-  useEffect(() => {
-    const today = new Date();
-    const formattedDate = today.toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    });
-    document.title = `DailyLIST - ${formattedDate}`;
-  }, []);
+    useEffect(() => {
+        const today = new Date();
+        const formattedDate = today.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        });
+        document.title = `DailyLIST - ${formattedDate}`;
+    }, []);
 
-  return (
-    <Router basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <Home />
-            </Layout>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <Layout>
-              <About />
-            </Layout>
-          }
-        />
-        <Route
-          path="/feedback"
-          element={
-            <Layout>
-              <Feedback />
-            </Layout>
-          }
-        />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router basename={import.meta.env.BASE_URL}>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Layout>
+                            <Home />
+                        </Layout>
+                    }
+                />
+                <Route
+                    path="/about"
+                    element={
+                        <Layout>
+                            <About />
+                        </Layout>
+                    }
+                />
+                <Route
+                    path="/feedback"
+                    element={
+                        <Layout>
+                            <Feedback />
+                        </Layout>
+                    }
+                />
+            </Routes>
+        </Router >
+    );
 }
 
 export default App;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
     return {
-        base: '/',
-        build: {
-            outDir: `../server/public/`,
-        },
+        base: env.VITE_BASE_PATH,
         plugins: [react(), tailwindcss()]
     }
 })
