2eb0170c83218ab382c778ad9f42b322dfc8123f
Derek Walker <d.walkerwd7@gmail.com>
2026-04-07 15:30:38 -0600

    apparently you do need to prefix apis and base path with the relative url that this project will be installed on if installing on another server

--- client/src/app-constants.ts
+++ client/src/app-constants.ts
@@ -1,5 +1,5 @@
 export const MAX_TODO_LIFETIME = 24 * 60 * 60 * 1000;
 export const APIPaths = {
-    todos: `/api/todos`,
-    feedback: `/api/feedback`
+    todos: `${import.meta.env.BASE_URL}/api/todos`,
+    feedback: `${import.meta.env.BASE_URL}/api/feedback`
 }
