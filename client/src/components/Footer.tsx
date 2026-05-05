3c6447073d9c1bf78e6c63b117a83558fee9b0e0
Derek Walker <d.walkerwd7@gmail.com>
2026-05-05 08:29:50 -0600

    a little rebranding for me

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,15 +1,15 @@
 export default function Footer() {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
-    return (
-        <footer className="border-t border-primary-border bg-primary-bg mt-8">
-            <div className="mx-auto max-w-4xl py-2 sm:px-6 lg:px-8">
-                <div className="flex flex-col items-center gap-6">
-                    <div className="text-center text-sm text-primary-text">
-                        <p>&copy; {currentYear} <a href="https://derekwalker.tech" target="_blank" className="text-sm text-indigo-500 hover:text-indigo-300">DTS</a>. MIT License.</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="border-t border-primary-border bg-primary-bg mt-8">
+      <div className="mx-auto max-w-4xl py-2 sm:px-6 lg:px-8">
+        <div className="flex flex-col items-center gap-6">
+          <div className="text-center text-sm text-primary-text">
+            <p>&copy; {currentYear} <a href="https://derekwalker.tech" target="_blank" className="text-sm text-indigo-500 hover:text-indigo-300">DTL</a>. MIT License.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
